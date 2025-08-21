d8cf8c68799207d37a3973dd23874179bc8bad13
AbdurRob <abdurrobr34l@gmail.com>
2025-08-21 21:26:50 +0600

    Completed this project, now i will just add few js code to improve its UX

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-function leapYear(year) {
+const LeapYearInput = document.getElementById('leap-year-input');
+const LeapYearResultInput = document.getElementById('leap-year-result-input');
 
-  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-    return (year + " is a Leap Year");
-  } else {
-    return (year + " is not a Leap Year");
-  }
+const LeapYearButton = document.getElementById('leap-year-btn').addEventListener('click', function () {
+  // console.log(LeapYearInput);  //? Checking if onclick is working when button is clicked
+  let gotYear = parseInt(LeapYearInput.value)  //? Saved user given year value
+  // console.log(gotYear);
   
-}
+  //? Now running the user given value inside of the condition to find out is it leap year/not
+  function leapYear(gotYear) {
 
-console.log("This method is 100% accurate--------");
-console.log(leapYear(1900));
-console.log(leapYear(1996));
-console.log(leapYear(2000));
+    if ((gotYear % 4 === 0 && gotYear % 100 !== 0) || gotYear % 400 === 0) {
+      // return (gotYear + " is a Leap Year");
+      return ("Is a Leap Year");
+    } else {
+      // return (gotYear + " is not a Leap Year");
+      return ("Is not a Leap Year");
+    }
+
+  }
+  const result = (leapYear(gotYear));  //? Saved condition result in this variable to show
+  // console.log(result)  //? Checking is condition value is saved/not
+
+LeapYearResultInput.value = result  //? Added the condition result inside the result input
+console.log(result);
+
+})
